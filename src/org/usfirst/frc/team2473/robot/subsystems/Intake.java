068806b1f981d4a67a6d9c0e6afd757e9fda4ac5
Wesley <wesleywa7@gmail.com>
2016-07-27 19:54:42 -0700

    Creating skeleton Intake subsystem

---
+++ src/org/usfirst/frc/team2473/robot/subsystems/Intake.java
@@ -0,0 +1,28 @@
+package org.usfirst.frc.team2473.robot.subsystems;
+
+import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import org.usfirst.frc.team2473.robot.RobotMap;
+
+
+/**
+ *
+ */
+public class Intake extends Subsystem {
+    
+	private SpeedController lowerIntake;
+	private SpeedController upperIntake;
+	
+	public Intake(){
+		lowerIntake = new CANTalon(RobotMap.leftBackMotor);
+	}
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
+
