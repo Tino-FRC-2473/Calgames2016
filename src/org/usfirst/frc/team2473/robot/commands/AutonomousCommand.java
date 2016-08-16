752ee8e5e8273bff542f6a757d5752a807e0d5ea
Deep Sethi <deeptanshu.146@gmail.com>
2016-08-15 20:48:27 -0700

    err

--- src/org/usfirst/frc/team2473/robot/commands/AutonomousCommand.java
+++ src/org/usfirst/frc/team2473/robot/commands/AutonomousCommand.java
@@ -5,14 +5,11 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class AutonomousCommand extends CommandGroup {
 
 	public AutonomousCommand() {
-<<<<<<< HEAD
 //		addSequential(new ForwardAutonomous());
 //		addSequential(new TurnAutonomous());
 		addSequential(new SensorReader());
 //		addSequential(new ForwardAutonomous());
-=======
 		addSequential(new StraightPIDCommand(StraightPIDCommand.FOREVER, 0.5));
->>>>>>> c5aa69fc4918bacaa4cd4e16a3a4a14d694e652f
 	}
 	
 	@Override
