0998e53b369270fb275b3cb748ddbfc500a129f9
thatSteveFan <pramukh.naduthota@gmail.com>
2016-08-17 19:14:53 -0700

    retrying pushing Database class again

--- src/org/usfirst/frc/team2473/robot/Database.java
+++ src/org/usfirst/frc/team2473/robot/Database.java
@@ -17,35 +17,85 @@ public class Database{
 	}
 
 
-	private volatile int gyroValue;
-	private ReentrantReadWriteLock gyroLock = new ReentrantReadWriteLock(true);
+	public static Database getInstance() {
+		return theInstance;
+	}
+
+	private ThreadSafeHolder gyroHolder = new ThreadSafeHolder();
 
 	public int getGyro() {
+		return gyroHolder.getValue();
+	}
+
+	public void setGyro(int newValue) {
+		gyroHolder.setValue(newValue);
+	}
+
+
+	private ThreadSafeHolder rightEncoderHolder = new ThreadSafeHolder();
+
+	public int getRightEncoder() {
+		return rightEncoderHolder.getValue();
+	}
+
+	public void setRightEncoder(int newValue) {
+		rightEncoderHolder.setValue(newValue);
+	}
+
+	private ThreadSafeHolder leftEncoderHolder = new ThreadSafeHolder();
+
+	public int getLeftEncoder() {
+		return leftEncoderHolder.getValue();
+	}
+
+	public void setLeftEncoder(int newValue) {
+		leftEncoderHolder.setValue(newValue);
+	}
+
+	private ThreadSafeHolder leftLightSensorHolder = new ThreadSafeHolder();
+
+	public int getLeftLightSensorHolder() {
+		return leftLightSensorHolder.getValue();
+	}
+
+	public void setLeftLightSensorHolder(int newValue) {
+		leftLightSensorHolder.setValue(newValue);
+	}
+
+	private ThreadSafeHolder rightLightSensorHolder = new ThreadSafeHolder();
+
+	public int getRightLightSensorHolder() {
+		return rightLightSensorHolder.getValue();
+	}
+
+	public void setRightLightSensorHolder(int newValue) {
+		rightLightSensorHolder.setValue(newValue);
+	}
+
+
+}
+
+class ThreadSafeHolder{
+
+	private volatile int value;
+	private ReentrantReadWriteLock lock = new ReentrantReadWriteLock(true);
+
+	public int getValue() {
 		try {
-			gyroLock.readLock().lock();
-			return gyroValue;
+			lock.readLock().lock();
+			return value;
 		} finally {
-			gyroLock.readLock().unlock();
+			lock.readLock().unlock();
 		}
 
 	}
 
-	public void setGyro(int newValue){
-		try{
-			gyroLock.writeLock().lock();
-			gyroValue = newValue;
-		}
-		finally {
-			gyroLock.writeLock().unlock();
+	public void setValue(int newValue) {
+		try {
+			lock.writeLock().lock();
+			value = newValue;
+		} finally {
+			lock.writeLock().unlock();
 		}
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-
 }
