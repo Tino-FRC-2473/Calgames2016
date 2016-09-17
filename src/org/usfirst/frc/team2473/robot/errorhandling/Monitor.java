c3e31b2018f3fed00edcc786783fd7f56264bcb6
RehanSD <RehanDurrani@10.19.87.124>
2016-09-16 18:06:14 -0700

    Created an abstract Monitor class that can be extended for anything that monitors. Also edited the RobotExceptionListener class.

---
+++ src/org/usfirst/frc/team2473/robot/errorhandling/Monitor.java
@@ -0,0 +1,10 @@
+package org.usfirst.frc.team2473.robot.errorhandling;
+
+public abstract class Monitor {
+	private RobotExceptionListener listener;
+	public Monitor(RobotExceptionListener l){
+		listener = l;
+	}
+	
+	public abstract void monitor(); //Must implement method l.exceptionThrown(RobotException e);
+}
--- src/org/usfirst/frc/team2473/robot/errorhandling/RobotExceptionListener.java
+++ src/org/usfirst/frc/team2473/robot/errorhandling/RobotExceptionListener.java
@@ -1,7 +1,10 @@
 package org.usfirst.frc.team2473.robot.errorhandling;
 
 import java.util.EventListener;
-
+/*
+ * Treat this like an ActionListener --> Check with Rehan on how to
+ * use ActionListeners
+ */
 public interface RobotExceptionListener extends EventListener{
 	public void ExceptionThrown(RobotException e);
 }
