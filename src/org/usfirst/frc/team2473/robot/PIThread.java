6763c56c984fdf71e3b0ea54bb00f224a165bda5
thatSteveFan <thatSteveFan@users.noreply.github.com>
2016-09-24 12:06:58 -0700

    Added PIThread class

--- src/org/usfirst/frc/team2473/robot/PIThread.java
+++ src/org/usfirst/frc/team2473/robot/PIThread.java
@@ -1,9 +1,14 @@
 package org.usfirst.frc.team2473.robot;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.net.URL;
 
 public class PIThread extends Thread {
@@ -13,35 +18,24 @@ public class PIThread extends Thread {
 
 	@Override
 	public void run() {
-		try {
-			URL url = new URL(PIAdress);
+		int portNumber = 69;
+		while (true) {
+			try (ServerSocket serverSocket = new ServerSocket(portNumber);
+					Socket clientSocket = serverSocket.accept();
+					PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+					BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));) {
 
-			HttpURLConnection connection = null;
+				String inputLine, outputLine;
 
-			while (connection == null) {
-				try {
-					connection = (HttpURLConnection) url.openConnection();
-				} catch (IOException expected) {
-					continue;
+				while ((inputLine = in.readLine()) != null) {
+					out.println(inputLine);
+					System.out.println(inputLine);
 				}
-
+			} catch (IOException e) {
+				System.out.println("Exception caught when trying to listen on port " + portNumber
+						+ " or listening for a connection");
+				System.out.println(e.getMessage());
 			}
-
-			connection.setRequestMethod("GET");
-			
-			
-			int code = connection.getResponseCode();
-			
-
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ProtocolException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
 
 	}
